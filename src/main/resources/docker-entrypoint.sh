b185a92a740478b11d7abc8ae8337634088e59c6
Ondro Mihalyi <mihalyi@omnifish.ee>
2023-08-17 01:04:06 +0200

    Use the startserv script as CMD by default. Add README with documentation for each docker tag. Signed-off-by:Ondro Mihalyi <mihalyi@omnifish.ee>

--- src/main/resources/docker-entrypoint.sh
+++ src/main/resources/docker-entrypoint.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 set -e;
 
-if [ "$1" != 'asadmin' ]; then
+if [ "$1" != 'asadmin' -a "$1" != 'startserv' ]; then
     exec "$@"
+fi
+
+CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
+if [ ! -f "$CONTAINER_ALREADY_STARTED" ]
+then
+    touch "$CONTAINER_ALREADY_STARTED" &&
+    rm -rf glassfish/domains/domain1/autodeploy/.autodeploystatus || true
+fi
+
+if [ "$1" == 'startserv' ]; then
+  exec "$@"
 fi
 
 on_exit () {
@@ -13,12 +24,5 @@ on_exit () {
     exit $EXIT_CODE;
 }
 trap on_exit EXIT
-
-CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
-if [ ! -f "$CONTAINER_ALREADY_STARTED" ]
-then
-    touch "$CONTAINER_ALREADY_STARTED" &&
-    rm -rf glassfish/domains/domain1/autodeploy/.autodeploystatus || true
-fi
 
 env|sort && "$@" & wait
