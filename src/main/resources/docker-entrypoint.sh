a4835975235f683118babfcbde859a77ff9c1495
Ondro Mihalyi <mihalyi@omnifish.ee>
2024-10-02 01:17:56 +0200

    Support for runnable GlassFish Embedded
    Using the static shell distribution of GF Embedded in GlassFish Server.
    Created autodeploy directory for GF Embedded for convenience.
    Documentation for GF Embedded.

--- src/main/resources/docker-entrypoint.sh
+++ src/main/resources/docker-entrypoint.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 set -e;
 
-if [ "$1" != 'asadmin' -a "$1" != 'startserv' ]; then
+if [ "$1" != 'asadmin' -a "$1" != 'startserv' -a "$1" != 'runembedded' ]; then
     exec "$@"
+fi
+
+if [ "$1" == 'runembedded' ]; then
+  shift 1
+  if [[ "$SUSPEND" == true ]]
+    then 
+      JVM_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=9009 $JVM_OPTS"
+  elif [[ "$DEBUG" == true ]]
+    then
+      JVM_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=9009 $JVM_OPTS"
+  fi
+  exec java $JVM_OPTS -jar glassfish/lib/embedded/glassfish-embedded-static-shell.jar "$@"
 fi
 
 CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
